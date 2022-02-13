321ba3b854493b8ff8b74b8ff027bbd95f107468
Nick Orth <nicholas.r.orth@gmail.com>
2022-02-13 16:42:35 -0600

    Add location to seeds file, add logic to check for image in show all camp page

--- seeds/index.js
+++ seeds/index.js
@@ -50,13 +50,10 @@ const seedDb = async () => {
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!",
       price: price,
+      geometry: { type: "Point", coordinates: [-115.1492, 36.1663] },
       images: [
         {
-          url: "https://res.cloudinary.com/dtk2pykqu/image/upload/v1644681546/YelpCamp/abd44dhftrcvmvk4inyn.png",
-          filename: "YelpCamp/abd44dhftrcvmvk4inyn",
-        },
-        {
-          url: "https://res.cloudinary.com/dtk2pykqu/image/upload/v1644681546/YelpCamp/abd44dhftrcvmvk4inyn.png",
+          url: "https://res.cloudinary.com/dtk2pykqu/image/upload/v1644780593/YelpCamp/asukqwgx6orqyr6xgx3q.jpg",
           filename: "YelpCamp/abd44dhftrcvmvk4inyn",
         },
       ],
